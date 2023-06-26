0350770de62b30e9ce9bf2dbfe4fec4390b43d1f
Narcoker <jykim9868@gmail.com>
2023-06-27 02:11:49 +0900

    Refactor: 코드 리펙토링

--- script.js
+++ script.js
@@ -1,59 +1,91 @@
-window.onload = () => puzzleInit(sideBarCards, boardCards);
-
 const sideBarCards = document.querySelectorAll(".sidebar .card img");
 const boardCards = document.querySelectorAll(".board .card img");
+const imageSrcs = [
+  "./src/images/deer_1.jpg",
+  "./src/images/deer_2.jpg",
+  "./src/images/deer_3.jpg",
+  "./src/images/deer_4.jpg",
+  "./src/images/deer_5.jpg",
+  "./src/images/deer_6.jpg",
+  "./src/images/deer_7.jpg",
+  "./src/images/deer_8.jpg",
+  "./src/images/deer_9.jpg",
+];
 
-// 드래그 이벤트 설정
-sideBarCards.forEach(setDragEvent);
-boardCards.forEach(setDragEvent);
+window.onload = () => puzzleInit(sideBarCards, boardCards);
 
-function setDragEvent(el) {
-  el.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("text/plain", event.target.id);
-  });
-  el.addEventListener("dragover", (event) => {
-    event.preventDefault();
-  });
-  el.addEventListener("drop", (event) => {
-    const id = event.dataTransfer.getData("text/plain");
-    const startEl = document.getElementById(id);
-    [event.target.src, startEl.src] = [startEl.src, event.target.src];
-    if (isClear()) {
-      endGame();
-    }
-  });
-}
+// 게임 초기화
+const puzzleInit = (sideBarCards, boardCards) => {
+  clearSidebar(sideBarCards);
+  clearBoard(boardCards);
+  setEventListeners(sideBarCards);
+  setEventListeners(boardCards);
+};
 
-// 퍼즐 진행 상황 체크
-function isClear() {
-  let result = true;
-  boardCards.forEach((el, i) => {
+const clearSidebar = () => {
+  imageSrcs.sort(() => Math.random() - 0.5);
+  sideBarCards.forEach((el, i) => {
+    el.src = imageSrcs[i];
+  });
+};
+
+const clearBoard = () => {
+  const blankImage = "./src/images/No_img.jpg";
+  boardCards.forEach((el) => {
+    el.src = blankImage;
+  });
+};
+
+const setEventListeners = (cards) => {
+  cards.forEach((card) => {
+    card.addEventListener("dragstart", dragStartFunc);
+    card.addEventListener("dragover", dragOverFunc);
+    card.addEventListener("drop", dropFunc);
+  });
+};
+
+const dragStartFunc = (event) => {
+  event.dataTransfer.setData("text/plain", event.target.id);
+};
+
+const dragOverFunc = (event) => {
+  event.preventDefault();
+};
+
+const dropFunc = (event) => {
+  const id = event.dataTransfer.getData("text/plain");
+  const startEl = document.getElementById(id);
+  [event.target.src, startEl.src] = [startEl.src, event.target.src];
+  if (isClear()) {
+    endGame();
+  }
+};
+
+// 정답 유무 체크
+const isClear = () => {
+  return [...boardCards].every((el, i) => {
     const fileName = el.src.split("/").pop();
     const fileIndex = Number(fileName.match(/\d+/)) - 1;
-    if (fileIndex != i) {
-      result = false;
-      return;
-    }
+    return fileIndex == i;
   });
-  return result;
-}
+};
 
 // 게임 종료
-function endGame() {
-  deleteDragEvent();
+const endGame = () => {
+  removeEventListener(boardCards);
+  removeEventListener(sideBarCards);
   showGameEndText();
-}
+};
 
-function deleteDragEvent() {
-  sideBarCards.forEach((el) => {
-    el.draggable = false;
-  });
-  boardCards.forEach((el) => {
-    el.draggable = false;
-  });
-}
+const removeEventListener = (cards) => {
+  cards.forEach(setDraggableFalse);
+};
 
-function showGameEndText() {
+const setDraggableFalse = (el) => {
+  el.draggable = false;
+};
+
+const showGameEndText = () => {
   const panelEl = document.querySelector(".panel");
   const button = document.querySelector('button[type="button"]');
   const congratulationText = document.createElement("p");
@@ -61,37 +93,4 @@ function showGameEndText() {
 
   panelEl.appendChild(congratulationText);
   button.style.display = "none";
-}
-
-// 게임 초기화
-function puzzleInit(sideBarCards, boardCards) {
-  clearSidebar(sideBarCards);
-  clearBoard(boardCards);
-}
-
-function clearSidebar() {
-  const imageSrcs = [
-    "./src/images/deer_1.jpg",
-    "./src/images/deer_2.jpg",
-    "./src/images/deer_3.jpg",
-    "./src/images/deer_4.jpg",
-    "./src/images/deer_5.jpg",
-    "./src/images/deer_6.jpg",
-    "./src/images/deer_7.jpg",
-    "./src/images/deer_8.jpg",
-    "./src/images/deer_9.jpg",
-  ];
-  imageSrcs.sort(() => Math.random() - 0.5);
-
-  sideBarCards.forEach((el, i) => {
-    el.src = imageSrcs[i];
-  });
-}
-
-function clearBoard() {
-  const blankImage = "./src/images/No_img.jpg";
-
-  boardCards.forEach((el) => {
-    el.src = blankImage;
-  });
-}
+};
