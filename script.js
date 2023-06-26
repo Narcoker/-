afe5b8f2594ce56e6dce5829ec72a0ab9c023868
Narcoker <jykim9868@gmail.com>
2023-06-27 00:11:57 +0900

    Feat: 정답 기능 구현 완료

--- script.js
+++ script.js
@@ -18,7 +18,49 @@ function setDragEvent(el) {
     const id = event.dataTransfer.getData("text/plain");
     const startEl = document.getElementById(id);
     [event.target.src, startEl.src] = [startEl.src, event.target.src];
+    if (isClear()) {
+      endGame();
+    }
   });
+}
+
+// 퍼즐 진행 상황 체크
+function isClear() {
+  let result = true;
+  boardCards.forEach((el, i) => {
+    const fileName = el.src.split("/").pop();
+    const fileIndex = Number(fileName.match(/\d+/)) - 1;
+    if (fileIndex != i) {
+      result = false;
+      return;
+    }
+  });
+  return result;
+}
+
+// 게임 종료
+function endGame() {
+  deleteDragEvent();
+  showGameEndText();
+}
+
+function deleteDragEvent() {
+  sideBarCards.forEach((el) => {
+    el.draggable = false;
+  });
+  boardCards.forEach((el) => {
+    el.draggable = false;
+  });
+}
+
+function showGameEndText() {
+  const panelEl = document.querySelector(".panel");
+  const button = document.querySelector('button[type="button"]');
+  const congratulationText = document.createElement("p");
+  congratulationText.textContent = "정답입니다.";
+
+  panelEl.appendChild(congratulationText);
+  button.style.display = "none";
 }
 
 // 게임 초기화
